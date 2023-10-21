1d464c4b403f1535178ba333f777db0f52542dff
Alessandro Carinelli <alessandro@Aspire-ES13.localdomain>
2023-10-21 09:36:03 +0200

    Revised Action

--- scripts/convert.py
+++ scripts/convert.py
@@ -3,4 +3,4 @@ from pyhtml2pdf import converter
 
 path = path = os.path.abspath('html/prova.html')
 print(f'File to convert: {path}')
-converter.convert(f'file:///{path}', '../pdf/prova.pdf')
+converter.convert(f'file:///{path}', 'pdf/prova.pdf')
